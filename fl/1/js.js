c505f0c7fc46cc709e74188c57b0020c6aea09ed
harut666 <aspt273@gmail.com>
2019-11-23 01:39:51 +0400

    добавил изменение бордера

--- fl/1/js.js
+++ fl/1/js.js
@@ -1,16 +1,23 @@
-let btn1,btn2,modal;
+let btn1,btn2,btn3,modal;
 
 btn1 = document.querySelector('#btn1');
 btn2 = document.querySelector('#btn2');
+btn3 = document.querySelector('#btn3');
 
 modal = document.querySelector('.modal');
 
 btn1.onclick = show;
 btn2.onclick = hide;
+btn3.onclick = border;
 
+hide();
 function show() {
 	modal.hidden = false;
 }
 function hide() {
 	modal.hidden =true;
+}
+function border(){
+	modal.style.border = '15px solid green';
+	modal.style.borderRadius='20px';
 }
